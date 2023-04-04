ae80c53fcf3098cba2543ec4349ab29547858fa1
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2023-04-04 22:45:27 +0100

    removed a redundant file

--- z.js
+++
@@ -1,8 +0,0 @@
-function tt(params) {
-    return 'func' in params ? params.func('hello world') : (
-        console.log('common error'),
-        console.error('not implemnted')
-    )
-}
-
-tt({ })
