3d66f4fa895e56e8f9ffba2fd3d7629281e6b4f6
Adeolaking <adeolakinga@gmail.com>
2023-06-02 10:28:35 +0100

    search functionality fixed with popup

--- src/components/sectionMedication/SectionMedication.jsx
+++ src/components/sectionMedication/SectionMedication.jsx
@@ -1,33 +1,24 @@
 import React, { useEffect, useState } from "react";
 import "./style.scss";
-// import Slide from "./slide";
-// import MedicationSearch from "./MedicationSearch";
+
 import { Users } from "./users";
 import axios from "../../utils/axios";
-import { data } from "autoprefixer";
-import { useActionData } from "react-router-dom";
-// import Slide from "./slide";
-
 
 function SectionMedication() {
   const [query, setQuery] = useState("");
   const [drug, setDrug] = useState([])
 
-  const searchQuery = ({users})=>{
 
-  }
+  console.log(drug)
   useEffect(()=>{
     axios
       .get("https://refillplug.up.railway.app/admin_view/list_medication/")
       .then((response) => {
-        console.log(response, typeof response);
         setDrug(response.data);
-        response.data.map((data) => console.log(data.name));
       })
       .catch((error) => console.log(error));
   },[])
-  
-console.log('drug', drug)
+
   return (
     <section className="medication">
       <h1 style={{ fontSize: "2rem", marginBottom: "1rem" }}>
@@ -51,7 +42,6 @@ console.log('drug', drug)
             >
               By Condition
             </div>
-
           </div>
 
           <div className="input-cont">
@@ -63,16 +53,15 @@ console.log('drug', drug)
           </div>
 
           <div className="slider">
-            {/* <Slide data={Users}/> */}
+         
             <div className="slider-01">
                  <ul>
               {
-                drug.map(data=><li key={data.id}>{data.name}</li>)
+                drug.filter((item)=> item.name.toLowerCase().includes(query.toLowerCase())).map(data=><li key={data.id}>{data.name }- { data.drug_form }</li>)
               }
             </ul>
             </div>
-         
-            
+   
             <div className="slider-02"></div>
           </div>
         </div>
