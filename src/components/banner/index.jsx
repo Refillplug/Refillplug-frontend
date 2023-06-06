8a4bf314bdda18e54707ec5c91ce372dd9a7711f
Imosemi <83042447+d-imosemi@users.noreply.github.com>
2023-06-06 21:48:09 +0100

    Update index.jsx

--- src/components/banner/index.jsx
+++ src/components/banner/index.jsx
@@ -8,8 +8,11 @@ export default function () {
     return <section id="banner">
         <div className="txt">
             <h2>
-                Do you need a personalised <span>prescription plan</span> or never run out of your medications?
+                Do you need a personalised <span>prescription plan</span>?
             </h2>
+            <h3>
+                    Never run out of your medications
+            </h3>
             <p>
                 Enjoy personalised medication plans with automated refills, free deliveries and ongoing support from healthcare Specialists
             </p>
