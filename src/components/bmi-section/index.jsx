519aba17178e70a2942fe0deda18008ba6689660
Adeolaking <adeolakinga@gmail.com>
2023-06-08 07:24:52 +0100

    development adjustment

--- src/components/bmi-section/index.jsx
+++ src/components/bmi-section/index.jsx
@@ -2,61 +2,89 @@ import React, { useEffect } from 'react'
 import { useState } from 'react';
 import './style.scss'
 import doctorImage from '../../assets/image/doctor.jpg';
+import axios from 'axios';
 
+import { data } from 'autoprefixer';
 
 
 function BmiSection() {
   
   const [height, setHeight] = useState(0);
-  const [mass, setMass] = useState(0);
+  const [weight, setWeight] = useState(0);
+  const [gender, setGender] = useState('');
   const [BMIResalt,setBMIResalt] = useState(0);
   const [ispopup, setIspopup] = useState(false);
+const [response, setResponse] = useState('')
 
-
-  function handleBMI(e){
-    e.preventDefault();
-    if (height && mass){
-      setBMIResalt( mass / ((height / 100) * (height / 100)));
+  // function handleBMI(e){
+  //   e.preventDefault();
+  //   if (height && mass){
+  //     setBMIResalt( mass / ((height / 100) * (height / 100)));
  
-      setIspopup(!ispopup)
-    }else{
-      setBMIResalt(0)
-    }
+  //     setIspopup(!ispopup)
+  //   }else{
+  //     setBMIResalt(0)
+  //   }
+  // }
+ 
+
+ 
+const calcBMI = async() => {
+    console.log(gender)
+  try{
+    const res = await axios.post(
+      "https://refillplug.up.railway.app/api/calculate-bmi/",
+      {
+        weight: weight,
+        height: height,
+        gender: "Male",
+      }
+    );
+    setIspopup(!ispopup); 
+    setResponse(res.data)
+    console.log(res.data ,'response' , res , response)
+  }catch(err){
+
   }
- 
 
- 
+   
+};
   
   return (
     <section className="flex gap-[2rem] p-2 ">
-      {ispopup && BMIResalt ? ( <>
-            <div className="modal max-w-xl max-h-sm  " id="popup">
-              <button
-                className="close-modal "
-                onClick={() => setIspopup(!ispopup)}
-              >
-                &times;
-              </button>
-              <h1 className="text-center">
-                BMI Resalt: {Math.round(BMIResalt)} kg/m
-                <sup>2</sup>
-              </h1>
-              <p>Note a healthy BMI range: 18.5 kg/m<sup>2</sup> - 25kg/m <sup>2</sup></p>
-            </div>
-            <div
-              className="overlay "
+      {ispopup ? (
+        <>
+          <div className="modal max-w-xl max-h-sm  " id="popup">
+            <button
+              className="close-modal "
               onClick={() => setIspopup(!ispopup)}
-            ></div>
-          </>) : null
-      }
+            >
+              &times;
+            </button>
+            <p>
+              <b>BMI:</b> {response.bmi}
+            </p>
+            <p>
+              <b>Classification:</b> {response.classification}
+            </p>
+            <p>
+              <b> Recommendation:</b> {response.recommendation}
+            </p>
+          </div>
+          <div className="overlay " onClick={() => setIspopup(!ispopup)}></div>
+        </>
+      ) : null}
 
-      
       <div className="image">
         <img src={doctorImage} alt="" />
       </div>
       <form
+        action="POST"
         id="form"
-        onSubmit={handleBMI}
+        onSubmit={(e) => {
+          e.preventDefault();
+          calcBMI();
+        }}
         style={{ boxShadow: "0em 0em 0em rgb(0 0 0 / 0%)" }}
       >
         <h1 style={{ fontWeight: "bold", fontSize: "1.5rem" }}>
@@ -66,11 +94,21 @@ function BmiSection() {
         <div className="row">
           <div className="row">
             <label htmlFor="male">Male</label>
-            <input type="radio" name="gender" id="male" />
+            <input
+              type="radio"
+              name="gender"
+              id="male"
+              onClick={(e) => setGender(e.target.value)}
+            />
           </div>
           <div className="row">
             <label htmlFor="female">Female</label>
-            <input type="radio" name="gender" id="female" />
+            <input
+              type="radio"
+              onChange={(e) => setGender(e.target.value)}
+              name="gender"
+              id="female"
+            />
           </div>
         </div>
 
@@ -78,7 +116,7 @@ function BmiSection() {
           <label htmlFor="">Weight</label>
           <input
             type="number"
-            onChange={(e) => setMass(Number(e.target.value))}
+            onChange={(e) => setWeight(Number(e.target.value))}
             placeholder="In kg"
             id="mass"
           />
